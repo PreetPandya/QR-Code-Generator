80c08b9d6a5c6c024534521f0b904fcf2154c75f
pandya3033 <pandyapreet456@gmail.com>
2023-11-03 15:52:54 +0530

    first commit

--- main.py
+++ main.py
@@ -1,48 +1,56 @@
 import streamlit as st
 import qrcode
 from io import BytesIO
-import base64
 
 st.title("QR CODE GENERATOR")
 
-link = st.text_input("Enter the URL:", placeholder="https://example.com")
+link = st.text_input("Enter The URL:", placeholder="https://example.com")
 
 if not link:
-    st.warning("PLEASE ENTER THE URL")
+    st.success("PLEASE ENTER THE URL")
+else:
+    def generate_qr_code(link):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=8,
+            )
+        qr.add_data(link)
+        qr.make(fit=True)
 
-def generate_qr_code(link):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=8,
-    )
-    qr.add_data(link)
-    qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_bytes = BytesIO()
+        img.save(img_bytes, format="PNG")
+        img_bytes = img_bytes.getvalue()
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    img_bytes = BytesIO()
-    img.save(img_bytes, format="PNG")
-    img_bytes = img_bytes.getvalue()
+        return img_bytes
 
-    st.image(img_bytes, use_column_width=True, caption="Generated QR Code")
-    img_base64 = base64.b64encode(img_bytes).decode()
-
-    return img_base64
-
-if st.button("Generate QR Code"):
-    img_base64 = generate_qr_code(link)
-
-    st.download_button(
-        label="Download QR Code",
-        data=img_base64,
-        file_name='qrcode.png',
-        key='download_qr_code',
-    )
+    if st.button("Generate QR Code"):
+            img_bytes = generate_qr_code(link)
+            st.image(img_bytes, use_column_width=True, caption="Generated QR Code")
+            st.download_button(
+            label="Download QR Code",
+            data=img_bytes,
+            file_name='qrcode.png',
+            key='download_qr_code',
+        )
 
 css = """
 <style>
 /* Add your custom CSS styles here */
+
+.stApp {
+            background-image: url('https://images.unsplash.com/photo-1621091211034-53136cc1eb32?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+            background-size: cover;
+            background-repeat: no-repeat;
+            font-family: 'Helvetica', sans-serif;
+    }
+    
+    /* Style the text input field */
+input[type="text"] {
+    background-color: black
+}
 
 /* Style the Generate QR Code button with light gray background */
 button {
@@ -66,6 +74,11 @@ div[data-baseweb="warning"] {
     background-color: transparent !important; /* Transparent background */
     margin: 10px 0 !important;
 }
+
+/* Hide Streamlit Branding */
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
 
 </style>
 """
